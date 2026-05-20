8047790c3da4a55df1516efd55c4dbc9e3dc4576
EzeWeber <ezeweber2003@gmail.com>
2026-05-20 12:44:27 -0300

    Acomodo schema turnos en su carpeta shifts y arreglo imports

--- BackEnd/app/api/v1/routes/shifts/turnos_router.py
+++ BackEnd/app/api/v1/routes/shifts/turnos_router.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import date
 
 from app.db.session import get_db
-from app.schemas.turno import (
+from BackEnd.app.schemas.shifts.turno import (
     TurnosDisponiblesResponse,
     TurnoDisponibleResponse,
 )
